53e2866711a75b5ba80e2fba7e56dc0cd3b91162
chmodder <hotmailgateway@gmail.com>
2013-12-19 10:46:43 +0100

    i gang med dropdown

--- landrupdans.Master.cs
+++ landrupdans.Master.cs
@@ -11,7 +11,9 @@ namespace landrupdans
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
         }
+
+
     }
 }
--- landrupdans.Master.designer.cs
+++ landrupdans.Master.designer.cs
@@ -31,6 +31,42 @@ namespace landrupdans {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// SearchRadioList control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButtonList SearchRadioList;
+        
+        /// <summary>
+        /// SearchBtn control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button SearchBtn;
+        
+        /// <summary>
+        /// SearchText control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox SearchText;
+        
+        /// <summary>
+        /// CreateUserPanel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel CreateUserPanel;
+        
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
