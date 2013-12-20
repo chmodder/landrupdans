5d5e02fbe37d91223702f43eae68656a3707799e
chmodder <hotmailgateway@gmail.com>
2013-12-20 09:44:28 +0100

    finshed dropdown menu

--- landrupdans.Master.designer.cs
+++ landrupdans.Master.designer.cs
@@ -58,15 +58,6 @@ namespace landrupdans {
         protected global::System.Web.UI.WebControls.TextBox SearchText;
         
         /// <summary>
-        /// CreateUserPanel control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Panel CreateUserPanel;
-        
-        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
